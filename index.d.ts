3a750e314051d091556666e2d02d1e7034452bda
Bence Knáb <benceknab@pm.me>
2020-11-26 15:58:38 +0100

    Refactor current methods to use new func template

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 /**
  * TODO:
  * - errors
- * - callbacks
+ * - replace unknown result types
  */
 
 declare module "ln-service" {
@@ -36,19 +36,35 @@ declare module "ln-service" {
   export type LightningNetworkService = LNService;
 
   export type Args<TArgs> = {
-    /** Authenticated LND API Object */
+    /** LND API Object */
     lnd: LND;
   } & TArgs;
-  export type AsyncRequest<TArgs, TResult> = (
-    args: Args<TArgs>
-  ) => Promise<TResult>;
-  export type RequestWithCallback<TArgs, TResult, TError = Error> = (
-    args: Args<TArgs>,
-    callback: (error: TError, result: TResult) => void
-  ) => void;
+
+  export type LNDMethod<TArgs, TResult = void, TError = Error> = {
+    (args: Args<TArgs>): Promise<TResult>;
+    (
+      args: Args<TArgs>,
+      callback: (error: TError, result: TResult) => void
+    ): void;
+  };
 
   const lnService: LNService;
   export default lnService;
+
+  export type AddPeerArgs = Args<{
+    /** Add Peer as Temporary Peer, default: `false` */
+    is_temporary?: boolean;
+    /** Public Key Hex */
+    public_key: string;
+    /** Retry Count */
+    retry_count?: number;
+    /** Delay Retry By Milliseconds */
+    retry_delay?: number;
+    /** Host Network Address And Optional Port, format: ip:port */
+    socket: string;
+    /** Connection Attempt Timeout Milliseconds */
+    timeout?: number;
+  }>;
 
   /**
    * Add a peer if possible (not self, or already connected)
@@ -56,120 +72,121 @@ declare module "ln-service" {
    * Requires `peers:write` permission
    *
    * `timeout` is not supported in LND 0.11.1 and below
-   *
-   * See: https://github.com/alexbosworth/ln-service#addpeer
    */
-  export function addPeer(variables: {
-    /** Add Peer as Temporary Peer Bool, default: `false` */
-    is_temporary?: boolean;
-    /** Authenticated LND API Object */
-    lnd: LND;
-    /** Public Key Hex String */
-    public_key: string;
-    /** Retry Count Number */
-    retry_count?: number;
-    /** Delay Retry By Milliseconds Number */
-    retry_delay?: number;
-    /** Host Network Address And Optional Port String>, ip:port */
-    socket: string;
-    /** Connection Attempt Timeout Milliseconds Number */
-    timeout?: number;
-  }): Promise<void>;
+  export const addPeer: LNDMethod<AddPeerArgs>;
 
-  export function addPeer(
-    variables: any,
-    callback: (err: Error, result: any) => void
-  );
+  export type BroadcastChainTransactionArgs = {
+    /** Transaction Label */
+    description?: string;
+    /** Transaction Hex */
+    transaction: string;
+  };
+
+  export type BroadcastChainTransactionResult = {
+    id: string;
+  };
 
   /**
    * Publish a raw blockchain transaction to Blockchain network peers
    *
    * Requires LND built with `walletrpc` tag
-   *
-   * See: https://github.com/alexbosworth/ln-service#broadcastchaintransaction
    */
-  export function broadcastChainTransaction(variables: {
-    /** Transaction Label String */
-    description?: string;
-
-    lnd: LND;
-    /** Transaction Hex String */
-    transaction: string;
-  }): Promise<{
-    /** Transaction Id Hex String */
-    id: string;
-  }>;
+  export const broadcastChainTransaction: LNDMethod<
+    BroadcastChainTransactionArgs,
+    BroadcastChainTransactionResult
+  >;
 
   export type ChannelPolicy = {
-    /** Base Fee Millitokens String */
+    /** Base Fee Millitokens */
     base_fee_mtokens: string;
-    /** CLTV Delta Number */
+    /** CLTV Delta */
     cltv_delta: number;
-    /** Fee Rate Number */
+    /** Fee Rate */
     fee_rate: number;
-    /** Channel is Disabled Bool */
+    /** Channel is Disabled */
     is_disabled: boolean;
-    /** Maximum HTLC Millitokens String */
+    /** Maximum HTLC Millitokens */
     max_htlc_mtokens: string;
-    /** Minimum HTLC Millitokens String */
+    /** Minimum HTLC Millitokens */
     min_htlc_mtokens: string;
-    /** Public Key Hex String */
+    /** Public Key Hex */
     public_key: string;
   };
+
   export type Channel = {
-    /** Capacity Tokens Number */
+    /** Capacity Tokens */
     capacity: number;
-    /** Standard Channel Id String */
+    /** Standard Channel Id */
     id: string;
     policies: ChannelPolicy[];
   };
 
   export type Hop = {
+    /** Base Fee Millitokens */
     base_fee_mtokens: string;
+    /** Standard Channel Id */
     channel: string;
+    /** Channel Capacity Tokens */
     channel_capacity: number;
+    /** CLTV Delta */
     cltv_delta: number;
+    /** Fee Rate */
     fee_rate: number;
+    /** Public Key Hex */
     public_key: string;
+  };
+
+  export type CalculateHopsArgs = {
+    channels: Channel[];
+    /** End Public Key Hex */
+    end: string;
+    ignore?: {
+      /** Standard Format Channel Id */
+      channel?: string;
+      /** Public Key Hex */
+      public_key: string;
+    }[];
+    /** Millitokens */
+    mtokens: number;
+    /** Start Public Key Hex */
+    start: string;
+  };
+
+  export type CalculateHopsResult = {
+    hops?: Hop[];
   };
 
   /**
    * Calculate hops between start and end nodes
-   *
-   * See: https://github.com/alexbosworth/ln-service#calculatehops
    */
-  export function calculateHops(variables: {
+  export const calculateHops: LNDMethod<CalculateHopsArgs, CalculateHopsResult>;
+
+  export type CalculatePathsArgs = {
     channels: Channel[];
-    /** End Public Key Hex String */
+    /** End Public Key Hex */
     end: string;
-    ignore?: {
-      /** Standard Format Channel Id String */
-      channel?: string;
-      /** Public Key Hex String */
-      public_key: string;
-    }[];
-    /** Millitokens Number */
+    /** Paths To Return Limit */
+    limit?: number;
+    /** Millitokens */
     mtokens: number;
-    /** Start Public Key Hex String */
+    /** Start Public Key Hex */
     start: string;
-  }): Promise<{ hops?: Hop[] }>;
+  };
+
+  export type CalculatePathsResult = { paths?: { hops: Hop[] }[] };
 
   /**
    * Calculate multiple routes to a destination
-   *
-   * See: https://github.com/alexbosworth/ln-service#calculatepaths
    */
-  export function calculatePaths(variables: {
-    channels: Channel[];
-    /** End Public Key Hex String */
-    end: string;
-    /** Paths To Return Limit Number */
-    limit?: number;
-    /** Millitokens Number */
-    mtokens: number;
-    /** Start Public Key Hex String */
-    start: string;
-  }): Promise<{ paths?: { hops: Hop[] }[] }>;
+  export const calculatePaths: LNDMethod<
+    CalculatePathsArgs,
+    CalculateHopsResult
+  >;
+
+  export type CancelHodlInvoiceArgs = {
+    /** Payment Preimage Hash Hex */
+    id: string;
+  };
 
   /**
    * Cancel an invoice
@@ -179,72 +196,81 @@ declare module "ln-service" {
    * Requires LND built with `invoicesrpc`
    *
    * Requires `invoices:write` permission
-   *
-   * See: https://github.com/alexbosworth/ln-service#cancelhodlinvoice
-   *
-   * TODO: return type?
    */
-  export function cancelHodlInvoice(variables: {
-    /** Payment Preimage Hash Hex String */
+  export const cancelHodlInvoice: LNDMethod<CancelHodlInvoiceArgs, unknown>;
+
+  export type CancelPendingChannelArgs = {
+    /** Pending Channel Id Hex */
     id: string;
-    /** Authenticated RPC LND API Object */
-    lnd: LND;
-  }): Promise<void>;
+  };
 
   /**
    * Cancel an external funding pending channel
-   *
-   * See: https://github.com/alexbosworth/ln-service#cancelpendingchannel
-   *
-   * TODO: return type?
    */
-  export function cancelPendingChannel(variables: {
-    /** Pending Channel Id Hex String */
-    id: string;
-    /** Authenticated LND API Object */
-    lnd: LND;
-  }): Promise<void>;
+  export const cancelPendingChannel: LNDMethod<
+    CancelPendingChannelArgs,
+    unknown
+  >;
+
+  export type ChangePasswordArgs = {
+    /** Current Password */
+    current_password: string;
+    /** New Password */
+    new_password: string;
+  };
 
   /**
    * Change wallet password
    *
    * Requires locked LND and unauthenticated LND connection
-   *
-   * See: https://github.com/alexbosworth/ln-service#changepassword
-   *
-   * TODO: return type?
    */
-  export function changePassword(variables: {
-    current_password: string;
-    lnd: LND;
-    new_password: string;
-  }): Promise<void>;
+  export const changePassword: LNDMethod<ChangePasswordArgs, unknown>;
+
+  export type CloseChannelArgs = {
+    /** Request Sending Local Channel Funds To Address */
+    address?: string;
+    /** Standard Format Channel Id */
+    id?: string;
+    /** Is Force Close */
+    is_force_close?: boolean;
+    /** Peer Public Key */
+    public_key?: string;
+    /** Peer Socket */
+    socket?: string;
+    /** Confirmation Target */
+    target_confirmations?: number;
+    /** Tokens Per Virtual Byte */
+    tokens_per_vbyte?: number;
+    /** Transaction Id Hex */
+    transaction_id?: string;
+    /** Transaction Output Index */
+    transaction_vout?: number;
+  };
+
+  export type CloseChannelResult = {
+    /** Closing Transaction Id Hex */
+    transaction_id: string;
+    /** Closing Transaction Vout */
+    transaction_vout: number;
+  };
 
   /**
    * Close a channel.
+   *
    * Either an id or a transaction id / transaction output index is required
    *
    * If cooperatively closing, pass a public key and socket to connect
    *
    * Requires `info:read`, `offchain:write`, `onchain:write`, `peers:write` permissions
-   *
-   * See: https://github.com/alexbosworth/ln-service#closechannel
    */
-  export function closeChannel(variables: {
-    address?: string;
-    id?: string;
-    is_force_close?: boolean;
-    lnd: LND;
-    public_key?: string;
-    socket?: string;
-    target_confirmations?: number;
-    tokens_per_vbyte?: number;
-    transaction_id?: string;
-    transaction_vout?: number;
-  }): Promise<{
-    transaction_id: string;
-    transaction_vout: number;
-  }>;
+  export const closeChannel: LNDMethod<CloseChannelArgs, CloseChannelResult>;
+
+  export type ConnectWatchtowerArgs = {
+    /** Watchtower Public Key Hex */
+    public_key: string;
+    /** Network Socket Address IP:PORT */
+    socket: string;
+  };
 
   /**
    * Connect to a watchtower
@@ -252,73 +278,50 @@ declare module "ln-service" {
    * This method requires LND built with `wtclientrpc` build tag
    *
    * Requires `offchain:write` permission
-   *
-   * See: https://github.com/alexbosworth/ln-service#connectwatchtower
-   *
-   * TODO: Return type?
    */
-  export function connectWatchtower(variables: {
-    /** Authenticated LND API Object */
-    lnd: LND;
-    /** Watchtower Public Key Hex String */
-    public_key: string;
-    /** Network Socket Address IP:PORT String */
-    socket: string;
-  }): Promise<void>;
+  export const connectWatchtower: LNDMethod<ConnectWatchtowerArgs, unknown>;
+
+  export type CreateChainAddressArgs = {
+    /** Receive Address Type */
+    format: "np2wpkh" | "p2wpkh";
+    /** Get As-Yet Unused Address */
+    is_unused?: boolean;
+  };
 
   /**
    * Create a new receive address.
    *
    * Requires address:write permission
-   *
-   * See: https://github.com/alexbosworth/ln-service#createchainaddress
-   *
-   * TODO: Return type?
    */
-  export function createChainAddress(variables: {
-    /** Receive Address Type String */
-    format: "np2wpkh" | "p2wpkh";
-    /** Get As-Yet Unused Address Bool */
-    is_unused?: boolean;
-    /** Authenticated LND API Object */
-    lnd: LND;
-  }): Promise<{ address: any }>;
+  export const createChainAddress: LNDMethod<
+    CreateChainAddressArgs,
+    { address: unknown }
+  >;
 
-  /**
-   * Create HODL invoice. This invoice will not settle automatically when an HTLC arrives. It must be settled separately with the secret preimage.
-   *
-   * Warning: make sure to cancel the created invoice before its CLTV timeout.
-   *
-   * Requires LND built with `invoicesrpc` tag
-   *
-   * Requires `address:write`, `invoices:write` permission
-   *
-   * See: https://github.com/alexbosworth/ln-service#createhodlinvoice
-   */
-  export function createHodlInvoice(variables: {
-    /** Final CLTV Delta Number */
+  export type CreateHodlInvoiceArgs = {
+    /** Final CLTV Delta */
     cltv_delta?: number;
-    /** Invoice Description String */
+    /** Invoice Description */
     description?: string;
-    /** Hashed Description of Payment Hex String */
+    /** Hashed Description of Payment Hex */
     description_hash?: string;
-    /** Expires At ISO 8601 Date String */
+    /** Expires At ISO 8601 Date */
     expires_at?: string;
-    /** Payment Hash Hex String */
+    /** Payment Hash Hex */
     id?: string;
-    /** Is Fallback Address Included Bool */
+    /** Is Fallback Address Included */
     is_fallback_included?: boolean;
-    /** Is Fallback Address Nested Bool */
+    /** Is Fallback Address Nested */
     is_fallback_nested?: boolean;
-    /** Invoice Includes Private Channels Bool */
+    /** Invoice Includes Private Channels */
     is_including_private_channels?: boolean;
-    /** Authenticated LND API Object */
-    lnd: LND;
-    /** Millitokens String */
+    /** Millitokens */
     mtokens?: string;
-    /** Tokens Number */
+    /** Tokens */
     tokens?: number;
-  }): Promise<{
+  };
+
+  export type CreateHodlInvoiceResult = {
     /** Backup Address String */
     chain_address?: string;
     /** ISO 8601 Date String */
@@ -335,5 +338,19 @@ declare module "ln-service" {
     secret?: string;
     /** Tokens Number */
     tokens: number;
-  }>;
+  };
+
+  /**
+   * Create HODL invoice. This invoice will not settle automatically when an HTLC arrives. It must be settled separately with the secret preimage.
+   *
+   * Warning: make sure to cancel the created invoice before its CLTV timeout.
+   *
+   * Requires LND built with `invoicesrpc` tag
+   *
+   * Requires `address:write`, `invoices:write` permission
+   */
+  export const createHodlInvoice: LNDMethod<
+    CreateHodlInvoiceArgs,
+    CreateHodlInvoiceResult
+  >;
 }
